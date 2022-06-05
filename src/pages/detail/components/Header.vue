0d4481e48313f4cf4725d59f03f921d2d9a58960
波罗罗 <Zhou.Jing21@outlook.com>
2022-06-05 22:22:49 +0800

    fix:修复问题

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -25,7 +25,10 @@ export default {
   },
   methods: {
     handleScroll() {
-      const top = document.documentElement.scrollTop;
+      const top =
+        document.documentElement.scrollTop ||
+        document.body.scrollTop ||
+        window.pageYOffset;
 
       if (top > 60) {
         let opacity = top / 140;
@@ -40,10 +43,10 @@ export default {
     }
   },
 
-  activated() {
+  mounted() {
     window.addEventListener("scroll", this.handleScroll);
   },
-  deactivated() {
+  destroyed() {
     window.removeEventListener("scroll", this.handleScroll);
   }
 };
